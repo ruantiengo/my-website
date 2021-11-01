f461aeda87ae4746a43edef3b4464ab0df7a022f
Ruan Tiengo <ruantiengo@gmail.com>
2021-11-01 18:45:21 -0300

    instagram added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.scss'
-import { ImFacebook, ImTwitter, ImYoutube, ImGithub, ImLinkedin } from 'react-icons/im'
+import { ImFacebook, ImTwitter, ImYoutube, ImGithub, ImLinkedin, ImInstagram } from 'react-icons/im'
+import { AiOutlineInstagram } from 'react-icons/ai'
 export function Footer() {
     return (
         <div className={styles.footerContainer}>
@@ -8,6 +9,11 @@ export function Footer() {
                     <a href="https://www.facebook.com/ruan.tiengo" className={styles.facebook} target="_blank" rel="noreferrer" >
                         <ImFacebook color="#fff" />
                     </a>
+
+                    <a href="https://www.instagram.com/tiengoruan/" className={styles.instagram} target="_blank" rel="noreferrer">
+                        <AiOutlineInstagram color="#fff" />
+                    </a>
+
                     <a href="https://www.twitter.com" className={styles.twitter} target="_blank" rel="noreferrer">
                         <ImTwitter color="#fff" />
                     </a>
@@ -20,6 +26,7 @@ export function Footer() {
                     <a href="https://www.linkedin.com/in/ruan-tiengo/" className={styles.linkedin} target="_blank" rel="noreferrer">
                         <ImLinkedin color="#fff" />
                     </a>
+
                 </div>
             </div>
             <div className={styles.autor}>
