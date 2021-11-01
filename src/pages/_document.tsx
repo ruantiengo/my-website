93881c9a52a3cf57817aafeff481a4d7b9f02b91
Ruan Tiengo <ruantiengo@gmail.com>
2021-11-01 18:51:04 -0300

    some update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,17 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-export default class MyDocument extends Document{
-    render(){
+export default class MyDocument extends Document {
+    render() {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Raleway:wght@400;700;900&display=swap" rel="stylesheet"/>
+                    <title>Ruan Tiengo</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Raleway:wght@400;700;900&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
-                    <Main/>
-                    <NextScript/>
+                    <Main />
+                    <NextScript />
                 </body>
             </Html>
         )
