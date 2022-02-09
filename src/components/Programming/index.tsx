e95545ef3d237bc00f051e1c6a8f21f7fc27314b
Ruan Tiengo <ruantiengo@Ruans-iMac.local>
2022-02-09 10:59:00 -0300

    fazer alterações

--- src/components/Programming/index.tsx
+++ src/components/Programming/index.tsx
@@ -129,7 +129,7 @@ export function Programming() {
                                     <div className={`${styles.percentLevel} ${styles.green} `}></div>
                                     <div className={`${styles.percentLevel} ${styles.green} `}></div>
                                     <div className={`${styles.percentLevel} ${styles.background} `}></div>
-
+                                    <div className={styles.percent}>80% </div>
                                 </div>
 
                             </div>
