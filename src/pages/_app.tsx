932742c7a3bb340feea88e852275e3d4e8ea72fa
Ruan Tiengo <ruantiengo@Ruans-iMac.local>
2022-02-09 12:39:20 -0300

    seo initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
+import { DefaultSeo } from 'next-seo';
 
 import type { AppProps } from 'next/app'
 import '../style/global.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <DefaultSeo {...{
+    title: 'Ruan Tiengo',
+    description: 'Sobre Ruan Tiengo',
+    canonical: 'https://ruantiengo.com',
+    openGraph:{
+        type: 'website',
+        locale: 'pt_BR',
+        url: 'https://ruantiengo.com'
+    }
+}}/>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
