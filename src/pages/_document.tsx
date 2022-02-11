d23784d2d20690eaea7912674d1b64bc2a5325f3
Ruan Tiengo <ruantiengo@Ruans-iMac.local>
2022-02-11 12:07:44 -0300

    some updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,10 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <title>Ruan Tiengo</title>
+                    
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Raleway:wght@400;700;900&display=swap" rel="stylesheet" />
-                    <meta name="description" content="Ruan Tiengo is a Software Engineer graduating at UFOP (Federal University of Ouro Preto). Currently working as a front end developer"></meta>
                     <meta name="keywords" content="Ruan Tiengo,rtiengo,tiengo,ruan-tiengo,software engineer,remote,software,flutter,ufop,developer,next,nextjs"></meta>
                 </Head>
                 <body>
