4dadff609decda997e0ef34abb90cb2d3ab85909
Ruan Tiengo <ruantiengo@Ruans-iMac.local>
2022-02-11 11:40:14 -0300

    updates in SEO

---
+++ next-seo-config.js
@@ -0,0 +1,13 @@
+const title = 'Ruan Tiengo';
+const description = "Hello, Ruan Tiengo here!";
+const SEO = {
+    title,
+    description,
+    canonical: 'https://ruantiengo.com',
+    openGraph:{
+        type: 'website',
+        locale: 'pt_BR',
+        url: 'https://ruantiengo.com'
+    }
+}
+
