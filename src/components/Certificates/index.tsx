81c0b1d45c40dc1122dd303faf5fccf54d5904a7
Ruan Tiengo <ruantiengo@gmail.com>
2022-03-03 09:02:20 -0300

    some updates

--- src/components/Certificates/index.tsx
+++ src/components/Certificates/index.tsx
@@ -1,31 +1,48 @@
-import styles from './styles.module.scss'
-import { Carousel } from 'react-bootstrap'
-import { useState } from 'react';
+/* eslint-disable @next/next/no-img-element */
+import styles from "./styles.module.scss";
+import { Carousel } from "react-bootstrap";
+import { useState } from "react";
 
 export function Certificates() {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex: any, e: any) => {
-        setIndex(selectedIndex);
-    };
-    return (
-        <div className={styles.certificatesContainer}>
-            <h1>My Certificates</h1>
-            <div className={styles.certificatesContent}>
-                <Carousel activeIndex={index} onSelect={handleSelect} variant="dark" interval={4000} pause={'hover'} >
-                    <Carousel.Item>
-                        <img src="/images/certificates/javascript.jpg" alt="First slide" className="d-block w-100" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/images/certificates/java11.jpg" alt="Second slide" className="d-block w-100" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/images/certificates/springboot.jpg" alt="Third slide" className="d-block w-100" />
-                    </Carousel.Item>
-                </Carousel>
-
-
-            </div>
-        </div>
-    );
+  const handleSelect = (selectedIndex: any, e: any) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <div className={styles.certificatesContainer}>
+      <h1>My Certificates</h1>
+      <div className={styles.certificatesContent}>
+        <Carousel
+          activeIndex={index}
+          onSelect={handleSelect}
+          variant="dark"
+          interval={4000}
+          pause={"hover"}
+        >
+          <Carousel.Item>
+            <img
+              src="/images/certificates/javascript.jpg"
+              alt="First slide"
+              className="d-block w-100"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="/images/certificates/java11.jpg"
+              alt="Second slide"
+              className="d-block w-100"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="/images/certificates/springboot.jpg"
+              alt="Third slide"
+              className="d-block w-100"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,39 +1,40 @@
-import styles from './styles.module.scss'
-import { BsMouse } from 'react-icons/bs'
-import  Image  from 'next/image'
+import styles from "./styles.module.scss";
+import { BsMouse } from "react-icons/bs";
+import Image from "next/image";
 
+export function Main() {
+  function getSmooth() {
+    const _mouseScr = document.querySelector("#mouseDonw");
+    _mouseScr?.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector("#mouse")?.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  }
 
-export function Main(){
-    function getSmooth(){
-        const _mouseScr= document.querySelector('#mouseDonw');
-        _mouseScr?.addEventListener('click', e => {
-            e.preventDefault();
-            document.querySelector("#mouse")?.scrollIntoView({
-                behavior: "smooth"
-            })
-        })
-    }
-    
-    return (
-        <div className={styles.mainContainer}>
-            <div className={styles.mainContent}>
-                    <div className={styles.personalPhoto}>
-                    <Image src='/images/pp.webp' width='200px' height='192px' className={styles.personalPhoto}/>
-                    </div>
-                    <span className={styles.introduction}>Hello, It's me</span> 
-                    <span className={styles.nameTitle}>Ruan Tiengo</span>
-                    <span className={styles.profession}>SOFTWARE ENGINEER</span>
-            </div>    
-            <div className={styles.mouse}>
-                        
-                        <a href='#mouse' id="mouseDonw" onClick={getSmooth}>
-                            <BsMouse size="30px" color="white"/>
-                        </a>
-            <script>
-                
-            </script>
-                        
-            </div>
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.mainContent}>
+        <div className={styles.personalPhoto}>
+          <Image
+            src="/images/pp.webp"
+            width="200px"
+            height="192px"
+            className={styles.personalPhoto}
+            alt="foto Ruan"
+          />
         </div>
-    )
+        <span className={styles.introduction}>Hello, It's me</span>
+        <span className={styles.nameTitle}>Ruan Tiengo</span>
+        <span className={styles.profession}>SOFTWARE ENGINEER</span>
+      </div>
+      <div className={styles.mouse}>
+        <a href="#mouse" id="mouseDonw" onClick={getSmooth}>
+          <BsMouse size="30px" color="white" />
+        </a>
+        <script></script>
+      </div>
+    </div>
+  );
 }
