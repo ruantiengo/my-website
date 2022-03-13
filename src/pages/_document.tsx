2cd1d2af72f736b66dffebb083a09e8c2d582bbe
Ruan Tiengo <ruantiengo@gmail.com>
2022-03-13 20:45:17 -0300

    google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,25 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Raleway:wght@400;700;900&display=swap" rel="stylesheet" />
-                    <meta name="keywords" content="Ruan Tiengo,rtiengo,tiengo,ruan-tiengo,software engineer,remote,software,flutter,ufop,developer,next,nextjs"></meta>
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Raleway:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="keywords"
+            content="Ruan Tiengo,rtiengo,tiengo,ruan-tiengo,software engineer,remote,software,flutter,ufop,developer,next,nextjs"
+          ></meta>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,17 @@
-import type { NextPage } from 'next'
-import { AboutMe } from '../components/AboutMe'
-import { Certificates } from '../components/Certificates'
-import { Footer } from '../components/Footer'
-import { Historic } from '../components/History'
-import { Main } from '../components/Main'
-import { Programming } from '../components/Programming'
-import { Repositories } from '../components/Repositories'
-import Meta from '../utils/Meta'
-
-
-
+import type { NextPage } from "next";
+import { AboutMe } from "../components/AboutMe";
+import { Certificates } from "../components/Certificates";
+import { Footer } from "../components/Footer";
+import { Historic } from "../components/History";
+import { Main } from "../components/Main";
+import { Programming } from "../components/Programming";
+import { Repositories } from "../components/Repositories";
+import Meta from "../utils/Meta";
 
 const Home: NextPage = (props) => {
-  
   return (
     <>
-      <Meta/>
+      <Meta />
       <Main />
       <AboutMe />
       <Repositories />
@@ -24,8 +20,7 @@ const Home: NextPage = (props) => {
       <Certificates />
       <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Home;
-
